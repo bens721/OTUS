2bc71a8abd53ce3c5717c037ca108473ffa184df
bens721 <67000973+bens721@users.noreply.github.com>
2022-11-13 21:23:49 +0200

    Update hw1.c
    - Исправления в алогиртме поиска имен файла
    - Удалена глобальная переменная

--- homeworks/hw1/hw1.c
+++ homeworks/hw1/hw1.c
@@ -2,27 +2,20 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define MAX_NAME_SIZE 255
-#define READ1B fread(&buffer, sizeof(uint8_t), 1, file)  
+#define READ1B fread(&buffer, sizeof(uint8_t), 1, file)
 #define ERROR_READ                                                             \
   printf("error to read file");                                                \
   exit(1)
 
-static uint8_t buffer; 
 
+bool file_names(FILE *file) {
 
-bool file_names(FILE *file, uint8_t buffer) {
+  uint8_t buffer;
+  size_t counter = 0;
+
   fseek(file, 0, SEEK_SET);
-  size_t counter = 0; 
-  uint16_t filename_len;
-  uint8_t *filename = (uint8_t *)calloc(MAX_NAME_SIZE, sizeof(uint8_t));
-  if (filename == NULL){
-    printf("allocated error");
-    exit(1); 
-    }
-  while (READ1B != 0) {   //чтение по одному байту, и сравнение с сигнатурой     
+  while (READ1B != 0) { //чтение по одному байту, и сравнение с сигнатурой
     if (buffer == 0x50) {
       if (READ1B <= 0) {
         ERROR_READ;
@@ -36,34 +29,39 @@ bool file_names(FILE *file, uint8_t buffer) {
             ERROR_READ;
           }
           if (buffer == 0x02) {
-            counter++;
-            fseek(file, 24, SEEK_CUR);    //смещаемся на 24 байта,для нахождения длины имени файла                                    
-             if (fread(&filename_len, sizeof(uint16_t), 1, file) <= 0) {
+            fseek(file, 24, SEEK_CUR); //смещаемся на 24 байта для определения
+                                       //размера имени файла
+            uint16_t filename_len;
+            if (fread(&filename_len, sizeof(uint16_t), 1, file) <= 0) {
               ERROR_READ;
-            }        
-              fseek(file, 16, SEEK_CUR);    //смещаемся на 16 байт,читаем имя, выводим на экран                                    
-              for (size_t i = 0; i <= filename_len; i++) {
-                filename[i] = fgetc(file);
-              }
-              printf("filename %lu: ", counter);
-              for (size_t i = 0; i <= filename_len; i++) {
-                printf("%c", filename[i]);
-              }
-              printf("\n");
-              memset(filename,'0',filename_len);                       
+            }
+            uint8_t filename[filename_len+1];
+            fseek(file, 16, SEEK_CUR); //смещаемся на 16 байт и читаем имя
+                                       //файла, выводим на экран
+            if (fread(filename, filename_len, 1, file) <= 0) {
+              ERROR_READ;
+            }
+            counter++;
+            printf("filename %lu: ", counter);
+            for (size_t i = 0; i < filename_len; i++) {   //Не пойму,почему если выводить через printf("%s",..),то выводится еще один непонятный символ.
+              //printf("%s",filename);
+              printf("%c",filename[i]);
+            }
+            printf("\n");
           }
         }
       }
     }
   }
-  free(filename);
   return true;
 }
 
-                                        
-bool zip_check(FILE *file, uint8_t buffer) { 
-    fseek(file, -200, SEEK_END); 
-  while (READ1B != 0) {   //чтение по одному байту, и сравнение с сигнатурой          
+
+bool zip_check(FILE *file) {
+  
+  uint8_t buffer = 0;
+  fseek(file, -200, SEEK_END); //ищем в конце файла нужную сигнатуру
+  while (READ1B != 0) { //чтение по одному байту, и сравнение с сигнатурой
     if (buffer == 0x50) {
       if (READ1B <= 0) {
         ERROR_READ;
@@ -87,9 +85,12 @@ bool zip_check(FILE *file, uint8_t buffer) {
 }
 
 
-bool jpg_check(FILE *file, uint8_t buffer) {
-  fseek(file, 0, SEEK_SET); 
-  if (READ1B <= 0) {    //чтение по одному байту, и сравнение с сигнатурой
+bool jpg_check(FILE *file) {
+  
+  uint8_t buffer = 0;
+  fseek(file, 0, SEEK_SET); //ищем в начале файла нужную сигнатуру
+
+  if (READ1B <= 0) { //чтение по одному байту, и сравнение с сигнатурой
     ERROR_READ;
   }
   if (buffer == 0xFF) {
@@ -97,7 +98,7 @@ bool jpg_check(FILE *file, uint8_t buffer) {
       ERROR_READ;
     }
     if (buffer == 0xD8) {
-      fseek(file, -2, SEEK_END);    //смещаемся в конец файла
+      fseek(file, -2, SEEK_END); //смещаемся в конец файла
       if (READ1B <= 0) {
         ERROR_READ;
       }
@@ -116,17 +117,18 @@ bool jpg_check(FILE *file, uint8_t buffer) {
 
 
 int main(int argc, char *argv[]) {
+  
   if (argc != 2) {
     printf("wrong the filename, try zipjpeg filename\n");
     exit(1);
   }
   FILE *file;
   if ((file = fopen(argv[1], "rb")) != NULL) {
-    if (jpg_check(file, buffer) == true && zip_check(file, buffer) == false)
+    if (jpg_check(file) == true && zip_check(file) == false)
       printf("This is jpg file\n");
-    if (jpg_check(file, buffer) == false && zip_check(file, buffer) == true)
-      file_names(file, buffer);
-    if (jpg_check(file, buffer) == false && zip_check(file, buffer) == false)
+    if (jpg_check(file) == false && zip_check(file) == true)
+      file_names(file);
+    if (jpg_check(file) == false && zip_check(file) == false)
       printf("unknown_file\n");
   }
   else {
